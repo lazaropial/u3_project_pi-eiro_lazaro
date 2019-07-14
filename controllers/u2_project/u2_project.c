99a35ec0aa29708f27272ad75f19f9a4aa9c4ccb
Lazaro piñeiro <al1609133@upy.edu.mx>
2019-07-14 18:19:17 -0500

    autonomous and manual mode done

--- controllers/u2_project/u2_project.c
+++ controllers/u2_project/u2_project.c
@@ -1,6 +1,6 @@
 /*
  * File: u2_exam_Pieiro_Lazaro.c
- * Date: 9 de julio del 2019      
+ * Date: 9 de julio del 2019
  * Description:
  * Author: Lazaro Piñeiro Alcocer
  * Modifications:
@@ -13,7 +13,7 @@
  #include <webots/keyboard.h>
 
  #include <stdio.h>
- 
+
  //#include <autonomous.h>
 
 /*
@@ -21,10 +21,11 @@
  */
  #define TIME_STEP 64
  #define PI 3.1416
- 
+
  enum {
  AUTOMATIC,
- MANUAL
+ MANUAL,
+ GO
  };
 
 /*variables globales*/
@@ -36,6 +37,7 @@
  int veces2=0;
  int m;
  short int robot_state;
+ int paro;
 
 /////////variables para el encoder///
  double pos_final, pos_final1, pos_final2;
@@ -46,71 +48,110 @@
  float rad=0.06; //radio de la llanta
  double vel, vel1,vel2;//velocidad lineal por llanta
  double vel_rob; //velocidad lineal del robot
- 
- 
- void autonomousMode (WbDeviceTag wheel1,WbDeviceTag wheel2,WbDeviceTag wheel3, WbDeviceTag ds_l, WbDeviceTag ds_r) {
- 
 
- 
-dl=(wb_distance_sensor_get_value(ds_l)*0.2)/65535;
+
+ void autonomousMode (WbDeviceTag wheel1,WbDeviceTag wheel2,WbDeviceTag wheel3, WbDeviceTag ds_l, WbDeviceTag ds_r) {
+
+
+
+  dl=(wb_distance_sensor_get_value(ds_l)*0.2)/65535;
   dr=(wb_distance_sensor_get_value(ds_r)*0.2)/65535;
-  
- 
- wb_motor_set_position(wheel1, INFINITY);
+
+  wb_motor_set_position(wheel1, INFINITY);
   wb_motor_set_velocity(wheel1, 0);
   wb_motor_set_position(wheel2, INFINITY);
   wb_motor_set_velocity(wheel2, -1);
   wb_motor_set_position(wheel3, INFINITY);
   wb_motor_set_velocity(wheel3, 1);
-  
-  
-        
+
+
+
     if (dl<=0.17 && dl<dr) {
       veces++;
     }
     if (veces>=1 && veces<=58) {
-      wb_motor_set_position(wheel1, INFINITY);
       wb_motor_set_velocity(wheel1, 1);
-      wb_motor_set_position(wheel2, INFINITY);
       wb_motor_set_velocity(wheel2, 1);
-      wb_motor_set_position(wheel3, INFINITY);
       wb_motor_set_velocity(wheel3, 1);
       veces++;
     }
-    
+
     else {
       veces=0;
     }
-       
+
     if (dr<0.17 && dr<dl) {
       veces2++;
     }
-     
-    if (veces2>=1 && veces2<=58) { 
-      wb_motor_set_position(wheel1, INFINITY);
+
+    if (veces2>=1 && veces2<=58) {
       wb_motor_set_velocity(wheel1, -1);
-      wb_motor_set_position(wheel2, INFINITY);
       wb_motor_set_velocity(wheel2, -1);
-      wb_motor_set_position(wheel3, INFINITY);
       wb_motor_set_velocity(wheel3, -1);
       veces2++;
     }
-     
+
     else {
       veces2=0;
     }
-  
+
  }
- 
- 
+
+ void stopRobot(WbDeviceTag wheel1,WbDeviceTag wheel2,WbDeviceTag wheel3) {
+   wb_motor_set_velocity(wheel1, 0);
+   wb_motor_set_velocity(wheel2, 0);
+   wb_motor_set_velocity(wheel3, 0);
+ }
+
+ void forwardRobot(WbDeviceTag wheel1,WbDeviceTag wheel2,WbDeviceTag wheel3) {
+   wb_motor_set_velocity(wheel1, 0);
+   wb_motor_set_velocity(wheel2, -1);
+   wb_motor_set_velocity(wheel3, 1);
+ }
+
+ void backwardRobot(WbDeviceTag wheel1,WbDeviceTag wheel2,WbDeviceTag wheel3) {
+   wb_motor_set_velocity(wheel1, 0);
+   wb_motor_set_velocity(wheel2, 1);
+   wb_motor_set_velocity(wheel3, -1);
+ }
+
+ void leftRobot(WbDeviceTag wheel1,WbDeviceTag wheel2,WbDeviceTag wheel3) {
+   wb_motor_set_velocity(wheel1, 0.9);
+   wb_motor_set_velocity(wheel2, -0.4);
+   wb_motor_set_velocity(wheel3, -0.4);
+ }
+
+ void rightRobot(WbDeviceTag wheel1,WbDeviceTag wheel2,WbDeviceTag wheel3) {
+   wb_motor_set_velocity(wheel1, -0.9);
+   wb_motor_set_velocity(wheel2, 0.4);
+   wb_motor_set_velocity(wheel3, 0.4);
+ }
+
+ void turnRightRobot(WbDeviceTag wheel1,WbDeviceTag wheel2,WbDeviceTag wheel3) {
+   wb_motor_set_velocity(wheel1, 1);
+   wb_motor_set_velocity(wheel2, 1);
+   wb_motor_set_velocity(wheel3, 1);
+ }
+
+ void turnLeftRobot(WbDeviceTag wheel1,WbDeviceTag wheel2,WbDeviceTag wheel3) {
+   wb_motor_set_velocity(wheel1, -1);
+   wb_motor_set_velocity(wheel2, -1);
+   wb_motor_set_velocity(wheel3, -1);
+ }
+
+
  int main(int argc, char **argv)
  {
-  wb_robot_init(); 
+  wb_robot_init();
 
   WbDeviceTag wheel1 = wb_robot_get_device("motor1");
   WbDeviceTag wheel2 = wb_robot_get_device("motor2");
-  WbDeviceTag wheel3 = wb_robot_get_device("motor3"); 
-   
+  WbDeviceTag wheel3 = wb_robot_get_device("motor3");
+
+  //wb_motor_set_position(wheel1, INFINITY);
+  //wb_motor_set_position(wheel2, INFINITY);
+  //wb_motor_set_position(wheel3, INFINITY);
+
   /////////position sensor//////////
   WbDeviceTag ps1 = wb_robot_get_device("pos1");
   WbDeviceTag ps2 = wb_robot_get_device("pos2");
@@ -118,89 +159,108 @@ dl=(wb_distance_sensor_get_value(ds_l)*0.2)/65535;
   wb_position_sensor_enable(ps1, TIME_STEP);
   wb_position_sensor_enable(ps2, TIME_STEP);
   wb_position_sensor_enable(ps3, TIME_STEP);
-   
+
   ///////////distance sensor/////////
   WbDeviceTag ds_l = wb_robot_get_device("distance_sensor");
-  WbDeviceTag ds_r = wb_robot_get_device("distance_sensor2");   
-  wb_distance_sensor_enable(ds_l, TIME_STEP);   
-  wb_distance_sensor_enable(ds_r, TIME_STEP); 
-  
+  WbDeviceTag ds_r = wb_robot_get_device("distance_sensor2");
+  wb_distance_sensor_enable(ds_l, TIME_STEP);
+  wb_distance_sensor_enable(ds_r, TIME_STEP);
+
   wb_keyboard_enable(TIME_STEP);
-  
+
   while (wb_robot_step(TIME_STEP) != -1) {
-  
-  //dl=(wb_distance_sensor_get_value(ds_l)*0.2)/65535;
-  //dr=(wb_distance_sensor_get_value(ds_r)*0.2)/65535;
-  
+
+  dl=(wb_distance_sensor_get_value(ds_l)*0.2)/65535;
+  dr=(wb_distance_sensor_get_value(ds_r)*0.2)/65535;
+
+
   ////rueda #1 //////////////
   a = wb_position_sensor_get_value(ps1);
   pos_final = ((a - b))/0.064;
   rpm= (pos_final*60)/(2*PI);
   b = a;
-          
+
   //////rueda #2////////////
   a1 = wb_position_sensor_get_value(ps2);
   pos_final1 = ((a1 - b1))/0.064;
   rpm1= (pos_final1*60)/(2*PI);
   b1 = a1;
-          
+
   //////rueda #3////////////
   a2 = wb_position_sensor_get_value(ps3);
   pos_final2 = ((a2 - b2))/0.064;
   rpm2 = (pos_final2*60)/(2*PI);
   b2 = a2;
-          
+
   /////////velocidad lineal del robot///////
   vel=pos_final*rad;
   vel1=pos_final1*rad;
   vel2=pos_final2*rad;
-  
+
     if (pos_final<0) {
       vel=vel*-1;
     }
-    
+
     if (pos_final1<0) {
       vel1=vel1*-1;
     }
-    
+
     if (pos_final2<0) {
       vel2=vel2*-1;
     }
-    
+
   vel_rob=(vel+vel1+vel2)/3;
-  
+
   /////////////movimiento del motor////////
   int key=wb_keyboard_get_key();
-  
-  
+
+
+
     if (key == 'G' || robot_state == AUTOMATIC) {
       //m=1;
-      
+
       autonomousMode(wheel1,wheel2,wheel3,ds_l,ds_r);
       robot_state = AUTOMATIC;
     }
-    
+
     if (key == 'W') {
       robot_state=MANUAL;
-      wb_motor_set_position(wheel1, INFINITY);
-      wb_motor_set_velocity(wheel1, 0);
-      wb_motor_set_position(wheel2, INFINITY);
-      wb_motor_set_velocity(wheel2, 0);
-      wb_motor_set_position(wheel3, INFINITY);
-      wb_motor_set_velocity(wheel3, 0);
-      
-       if (key == WB_KEYBOARD_LEFT) {
-         wb_motor_set_position(wheel1, INFINITY);
-         wb_motor_set_velocity(wheel1, 1);
-         wb_motor_set_position(wheel2, INFINITY);
-         wb_motor_set_velocity(wheel2, -1);
-         wb_motor_set_position(wheel3, INFINITY);
-         wb_motor_set_velocity(wheel3, 0);
-       }
+      stopRobot(wheel1,wheel2,wheel3);
+     }
+    if (key == WB_KEYBOARD_LEFT && robot_state == MANUAL) {
+      paro=key;
+      leftRobot(wheel1,wheel2,wheel3);
     }
-         
-  printf("MODO: %i\tRPM_1= %f RPM\tRPM_2= %f RPM\tRPM_3= %f RPM\tlinear velocity= %f m/s\tds_l=%f\tds_r=%f\t%i\n",robot_state,rpm,rpm1,rpm2,vel_rob,dl,dr,veces);
-    
+
+    if (paro != key && robot_state == MANUAL) {
+      stopRobot(wheel1,wheel2,wheel3);
+    }
+
+    if (key==WB_KEYBOARD_UP && robot_state == MANUAL) {
+      forwardRobot(wheel1,wheel2,wheel3);
+    }
+
+    if(key==WB_KEYBOARD_DOWN && robot_state == MANUAL) {
+      backwardRobot(wheel1,wheel2,wheel3);
+    }
+
+    if(key==WB_KEYBOARD_RIGHT && robot_state == MANUAL) {
+      rightRobot(wheel1,wheel2,wheel3);
+    }
+
+    if(key=='A' && robot_state == MANUAL) {
+      turnLeftRobot(wheel1,wheel2,wheel3);
+    }
+
+    if(key=='S' && robot_state == MANUAL) {
+      turnRightRobot(wheel1,wheel2,wheel3);
+    }
+
+
+  printf("MODO: %i\tRPM_1= %f RPM\tRPM_2= %f RPM\tRPM_3= %f RPM\
+  linear velocity= %f m/s\tds_l=%f\tds_r=%f\t%i\n"
+  ,robot_state,rpm,rpm1,rpm2,vel_rob,dl,dr,veces);
+
   };
 
   wb_robot_cleanup();
